62e6d7c62bfd92181c6c2ea574f746d9c9f7156f
Damien Le Moal <damien.lemoal@hgst.com>
2015-06-15 14:10:49 +0900

    Removed wrong "confidential" header.

--- tools/lkvs/autogen.sh
+++ tools/lkvs/autogen.sh
@@ -1,10 +1,3 @@
 #!/bin/sh
 
-# (C) Copyright 2014 HGST, a Western Digital Company, 
-# San Jose Research Center All Rights Reserved
-#
-# All information contained herein is proprietary and confidential to HGST. 
-# Any use, reproduction, or disclosure without written permission to HGST 
-# is prohibted. 
-
 autoreconf -vfi
