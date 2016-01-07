15b66b4e6973516250963c4a18e4259a8cde385a
masato.suzuki <masato.suzuki@hgst.com>
2016-01-07 14:32:55 +0900

    Modified program and scripts to support revision 04.

---
+++ test/scripts/00_command_completion_check/011.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+#
+# This file is part of libzbc.
+#
+# Copyright (C) 2009-2014, HGST, Inc.  All rights reserved.
+#
+# This software is distributed under the terms of the BSD 2-clause license,
+# "as is," without technical support, and WITHOUT ANY WARRANTY, without
+# even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+# PURPOSE. You should have received a copy of the BSD 2-clause license along
+# with libzbc. If not, see  <http://opensource.org/licenses/BSD-2-Clause>.
+#
+
+. ../zbc_test_lib.sh
+
+zbc_test_init $0 $*
+
+zbc_test_info "REPORT_ZONES (partial bit) command completion..."
+
+# Set expected error code
+expected_sk=""
+expected_asc=""
+
+# Get drive information
+zbc_test_get_drive_info
+
+# Set target LBA
+target_lba="0"
+
+# Start testing
+zbc_test_run ${bin_path}/zbc_test_report_zones -v -p -lba ${target_lba} ${device}
+
+# Check result
+zbc_test_get_sk_ascq
+zbc_test_check_no_sk_ascq
+
+# Check failed
+zbc_test_check_failed
+
