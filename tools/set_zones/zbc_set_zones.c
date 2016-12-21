fd3ed9656b02ef630300926691879f0f14d1dccf
Damien Le Moal <damien.lemoal@wdc.com>
2016-12-21 11:43:00 +0900

    Set zones tool: Use zbc_print_device_info
    Also fix zone size chekcs to properly use 512B sector unit.
    
    Signed-off-by: Damien Le Moal <damien.lemoal@wdc.com>

--- tools/set_zones/zbc_set_zones.c
+++ tools/set_zones/zbc_set_zones.c
@@ -51,7 +51,7 @@ usage:
 	}
 
 	/* Parse options */
-	for (i = 1; i < (argc - 2); i++) {
+	for (i = 1; i < argc - 3; i++) {
 
 		if (strcmp(argv[i], "-v") == 0) {
 
@@ -70,42 +70,25 @@ usage:
 
 	}
 
-	if (i > (argc - 2))
+	if (i > argc - 3)
 		goto usage;
 
 	/* Open device */
 	path = argv[i];
 	ret = zbc_open(path, O_RDONLY, &dev);
 	if (ret < 0) {
-		if (ret == -ENXIO) {
+		if (ret == -ENXIO)
 			fprintf(stderr, "Unsupported device type\n");
-		} else {
+		else
 			fprintf(stderr,
 				"zbc_open failed %d (%s)\n",
 				ret, strerror(-ret));
-		}
 		return 1;
 	}
 
 	/* Get device info */
 	zbc_get_device_info(dev, &info);
-
-	printf("Device %s: %s\n",
-	       path,
-	       info.zbd_vendor_id);
-	printf("    %s interface, %s disk model\n",
-	       zbc_disk_type_str(info.zbd_type),
-	       zbc_disk_model_str(info.zbd_model));
-	printf("    %llu 512-bytes sectors\n",
-	       (unsigned long long) info.zbd_sectors);
-	printf("    %llu logical blocks of %u B\n",
-	       (unsigned long long)info.zbd_lblocks,
-	       (unsigned int)info.zbd_lblock_size);
-	printf("    %llu physical blocks of %u B\n",
-	       (unsigned long long)info.zbd_pblocks,
-	       (unsigned int)info.zbd_pblock_size);
-	printf("    %.03F GB capacity\n",
-	       (double)(info.zbd_sectors << 9) / 1000000000);
+	zbc_print_device_info(&info, path, stdout);
 	printf("\n");
 
 	/* Process command */
@@ -114,13 +97,14 @@ usage:
 
 	if (strcmp(argv[i], "set_sz") == 0) {
 
-		/* Set size */
-		if (i != (argc - 3))
+		/*
+		 * Set conventional zones capacity and zone size for all zones.
+		 */
+		if (i != argc - 3)
 			goto usage;
 
 		/* Get arguments */
-		conv_sz = (strtoll(argv[i + 1], NULL, 10) * 1024 * 1024)
-			/ info.zbd_lblock_size;
+		conv_sz = (strtoll(argv[i + 1], NULL, 10) * 1024 * 1024) >> 9;
 		if (conv_sz < 0) {
 			fprintf(stderr, "Invalid conventional zones size %s\n",
 				argv[i + 1]);
@@ -129,7 +113,7 @@ usage:
 		}
 
 		zone_sz = (strtoll(argv[i + 2], NULL, 10) * 1024 * 1024)
-			/ info.zbd_lblock_size;
+			>> 9;
 		if (zone_sz <= 0) {
 			fprintf(stderr, "Invalid zone size %s\n",
 				argv[i + 2]);
@@ -139,8 +123,11 @@ usage:
 
 	} else if (strcmp(argv[i], "set_ps") == 0) {
 
-		/* Set perc + num */
-		if (i != (argc - 3))
+		/*
+		 * Set conventional zones capacity percentage and
+		 * zone size for all zones.
+		 */
+		if (i != argc - 3)
 			goto usage;
 
 		/* Get arguments */
@@ -151,18 +138,16 @@ usage:
 			ret = 1;
 			goto out;
 		}
-
-		conv_sz = (long long)((double) info.zbd_lblocks * (double) conv_p) / 100;
+		conv_sz = (long long)((double) info.zbd_sectors * (double) conv_p) / 100;
 
 		zone_sz = (strtoll(argv[i + 2], NULL, 10) * 1024ULL * 1024ULL)
-			/ info.zbd_lblock_size;
-		if (zone_sz == 0) {
+			>> 9;
+		if (zone_sz <= 0) {
 			fprintf(stderr, "Invalid zone size %s\n",
 				argv[i + 2]);
 			ret = 1;
 			goto out;
 		}
-
 
 	} else {
 
@@ -171,15 +156,27 @@ usage:
 
 	}
 
+	if ((unsigned long long)conv_sz >= info.zbd_sectors) {
+		fprintf(stderr,
+			"Invalid conventional zone capacity (too large)\n");
+		ret = 1;
+		goto out;
+	}
+	if (conv_sz && conv_sz < zone_sz) {
+		fprintf(stderr,
+			"Invalid conventional zone capacity (too low)\n");
+		ret = 1;
+		goto out;
+	}
+
 	if (conv_sz) {
 		conv_num = conv_sz / zone_sz;
 		if (!conv_num || conv_sz % zone_sz)
 			conv_num++;
+		conv_sz = zone_sz * conv_num;
 	} else {
 		conv_num = 0;
 	}
-	conv_sz = (zone_sz * conv_num) >> 9;
-	zone_sz >>= 9;
 
 	printf("    Zone size: %lld MiB (%lld sectors)\n",
 	       (zone_sz << 9) / (1024 * 1024),
@@ -195,7 +192,7 @@ usage:
 	       (info.zbd_sectors - conv_sz) / zone_sz);
 
 	ret = zbc_set_zones(dev, conv_sz, zone_sz);
-	if ( ret != 0 ) {
+	if (ret != 0) {
 		fprintf(stderr,
 			"zbc_set_zones failed %d (%s)\n",
 			ret,
