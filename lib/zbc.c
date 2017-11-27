43e3435009102a084d743e37d6915d35adbdbb57
David Butterfield <dab21774@gmail.com>
2017-11-26 19:36:48 -0700

    fix corrupt buffer address when sector count > max
    Change zbc_pread/zbc_pwrite to add to the buffer address the number of bytes
    read/written rather than something else.  This only affects read/write ops
    whose total sector count > zbd_max_rw_sectors.

--- lib/zbc.c
+++ lib/zbc.c
@@ -560,7 +560,7 @@ ssize_t zbc_pread(struct zbc_device *dev, void *buf,
 			return ret ? ret : -EIO;
 		}
 
-		buf += ret << 9;
+		buf += ret;
 		offset += ret;
 		count -= ret;
 		rd_count += ret;
@@ -615,7 +615,7 @@ ssize_t zbc_pwrite(struct zbc_device *dev, const void *buf,
 			return ret ? ret : -EIO;
 		}
 
-		buf += ret << 9;
+		buf += ret;
 		offset += ret;
 		count -= ret;
 		wr_count += ret;
