4abfb04947931064b1357d32695628b3c97b4d56
Dmitry Fomichev <dmitry.fomichev@wdc.com>
2018-01-03 18:44:27 -0500

    Debug realm utilities

--- tools/convert_realms/zbc_convert_realms.c
+++ tools/convert_realms/zbc_convert_realms.c
@@ -21,44 +21,63 @@
 
 #include <libzbc/zbc.h>
 
+#define ZBC_O_DRV_MASK (ZBC_O_DRV_BLOCK | ZBC_O_DRV_SCSI | \
+			ZBC_O_DRV_ATA | ZBC_O_DRV_FAKE)
+
 int main(int argc, char **argv)
 {
 	struct zbc_device_info info;
 	struct zbc_device *dev;
 	uint64_t start_realm;
 	unsigned int nr_realms;
-	int i, type, fg = 0, ret = 1;
+	int i, type, fg = 0, ret = 1, flags = 0;
 	char *path;
 
 	/* Check command line */
 	if (argc < 5) {
+		fprintf(stderr, "Not enough arguments\n");
 usage:
 		printf("Usage: %s [options] <dev> <start realm> "
-		       "<num realms> <zone type>[ <fg>]\n"
+		       "<num realms> <new zone type>[ <fg>]\n"
+		       "New zone types:\n"
+		       "    1             : conventional\n"
+		       "    2             : sequential write required\n"
+		       "    3             : sequential write preferred\n"
 		       "Options:\n"
-		       "    -v   : Verbose mode\n",
+		       "    -v            : Verbose mode\n"
+		       "    -f <hex mask> : Use the specified backend mask"
+		       " to open the device\n",
 		       argv[0]);
 		return 1;
 	}
 
 	/* Parse options */
 	for (i = 1; i < (argc - 1); i++) {
-
-		if ( strcmp(argv[i], "-v") == 0 ) {
-
+		if ( strcmp(argv[i], "-v") == 0 )
 			zbc_set_log_level("debug");
+		else if (strcmp(argv[i], "-f") == 0) {
+			if (i >= (argc - 1)) {
+				fprintf(stderr, "Missing backend flag value\n");
+				goto usage;
+			}
+			i++;
 
-		} else if ( argv[i][0] == '-' ) {
-
-			printf("Unknown option \"%s\"\n", argv[i]);
-			return 1;
-
-		} else {
-
-			break;
-
+			flags = strtol(argv[i], NULL, 16);
+			if (flags != 0) {
+				if (flags < 0) {
+					fprintf(stderr, "Invalid flag value %i\n",
+						flags);
+					goto usage;
+				}
+				flags &= ZBC_O_DRV_MASK;
+			}
 		}
-
+		else if ( argv[i][0] == '-' ) {
+			fprintf(stderr, "Unknown option \"%s\"\n", argv[i]);
+			goto usage;
+		}
+		else
+			break;
 	}
 
 	if (i >= argc) {
@@ -81,11 +100,18 @@ usage:
 		goto usage;
 	}
 	type = atoi(argv[i++]);
+	if (type != ZBC_ZT_CONVENTIONAL &&
+	    type != ZBC_ZT_SEQUENTIAL_REQ &&
+	    type != ZBC_ZT_SEQUENTIAL_PREF) {
+		fprintf(stderr, "Invalid new zone type %i\n", type);
+		goto usage;
+	}
+
 	if (i < argc)
 		fg = atoi(argv[i]);
 
 	/* Open device */
-	ret = zbc_open(path, O_RDWR, &dev);
+	ret = zbc_open(path, flags | O_RDWR, &dev);
 	if (ret != 0)
 		return 1;
 
