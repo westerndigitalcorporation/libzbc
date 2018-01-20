0f5fea148c2b275c996fe0c34ff1e517074f9403
Damien Le Moal <damien.lemoal@wdc.com>
2018-01-21 03:58:27 +0900

    Simplified report realms
    Do not loop trying different backend when opening the device: zbc_open
    does this automatically. Detection of "realm device" should be added
    after getting the device information.
    
    Signed-off-by: Damien Le Moal <damien.lemoal@wdc.com>

--- tools/report_realms/zbc_report_realms.c
+++ tools/report_realms/zbc_report_realms.c
@@ -21,8 +21,7 @@
 
 #include <libzbc/zbc.h>
 
-#define ZBC_O_DRV_MASK (ZBC_O_DRV_BLOCK | ZBC_O_DRV_SCSI | \
-			ZBC_O_DRV_ATA | ZBC_O_DRV_FAKE)
+#define ZBC_O_DRV_MASK (ZBC_O_DRV_BLOCK | ZBC_O_DRV_SCSI | ZBC_O_DRV_ATA)
 
 static void zbc_report_print_realm(struct zbc_device_info *info,
 				   struct zbc_realm *r)
@@ -50,8 +49,8 @@ int main(int argc, char **argv)
 	struct zbc_device_info info;
 	struct zbc_device *dev;
 	unsigned int nr_realms = 0, nr = 0;
-	struct zbc_realm *r, *realms = NULL;
-	int i, ret = 1, num = 0, flags = 0;
+	struct zbc_realm *realms = NULL;
+	int i, ret = 1, num = 0;
 	char *path;
 
 	/* Check command line */
@@ -60,8 +59,6 @@ usage:
 		printf("Usage: %s [options] <dev>\n"
 		       "Options:\n"
 		       "  -v		  : Verbose mode\n"
-		       "  -f <hex mask>   : Use the specified backend mask"
-		       " to open the device\n"
 		       "  -n		  : Get only the number of realms\n"
 		       "  -nr <num>	  : Get at most <num> realms\n",
 		       argv[0]);
@@ -70,11 +67,12 @@ usage:
 
 	/* Parse options */
 	for (i = 1; i < (argc - 1); i++) {
-		if (strcmp(argv[i], "-v") == 0)
+
+		if (strcmp(argv[i], "-v") == 0) {
 			zbc_set_log_level("debug");
-		else if (strcmp(argv[i], "-n") == 0)
+		} else if (strcmp(argv[i], "-n") == 0) {
 			num = 1;
-		else if (strcmp(argv[i], "-nr") == 0) {
+		} else if (strcmp(argv[i], "-nr") == 0) {
 			if (i >= (argc - 1))
 				goto usage;
 			i++;
@@ -82,103 +80,71 @@ usage:
 			nr = strtol(argv[i], NULL, 10);
 			if (nr <= 0)
 				goto usage;
-		}
-		else if (strcmp(argv[i], "-f") == 0) {
-			if (i >= (argc - 1)) {
-				fprintf(stderr, "Missing backend flag value\n");
-				goto usage;
-			}
-			i++;
-
-			flags = strtol(argv[i], NULL, 16);
-			if (flags != 0) {
-				if (flags < 0) {
-					fprintf(stderr, "Invalid backend flag %i\n",
-						flags);
-					goto usage;
-				}
-				flags &= ZBC_O_DRV_MASK;
-			}
-		}
-		else if (argv[i][0] == '-') {
+		} else if (argv[i][0] == '-') {
 			fprintf(stderr, "Unknown option \"%s\"\n",
 				argv[i]);
 			goto usage;
-		}
-		else
+		} else {
 			break;
+		}
+
 	}
 
 	if (i != (argc - 1))
 		goto usage;
+	path = argv[i];
 
 	/* Open device */
-	path = argv[i];
-	while (1) {
-		ret = zbc_open(path, flags | O_RDONLY, &dev);
-		if (ret != 0)
-			return 1;
+	ret = zbc_open(path, ZBC_O_DRV_MASK | O_RDONLY, &dev);
+	if (ret != 0)
+		return 1;
 
-		zbc_get_device_info(dev, &info);
+	zbc_get_device_info(dev, &info);
 
-		printf("Device %s:\n", path);
-		zbc_print_device_info(&info, stdout);
+	printf("Device %s:\n", path);
+	zbc_print_device_info(&info, stdout);
 
-		/* We can skip zbc_report_nr_realms() call if we receive
-		 * the number from SCSI INQUIRY or ATA LOG page */
-		if (info.zbd_realm_list_length == 0) {
-			ret = zbc_report_nr_realms(dev, &nr_realms);
-			if (ret != 0) {
-				fprintf(stderr, "zbc_report_nr_realms failed %d\n",
-					ret);
-				ret = 1;
-				goto out;
-			}
-		}
-		else
-			nr_realms = info.zbd_realm_list_length;
-
-		printf("    %u realm%s\n", nr_realms, (nr_realms > 1) ? "s" : "");
-		if (num)
-			goto out;
-
-		if (!nr || nr > nr_realms)
-			nr = nr_realms;
-		if (!nr)
-			goto out;
-
-		/* Allocate realm array */
-		if (!realms)
-			realms = (struct zbc_realm *)calloc(nr, sizeof(struct zbc_realm));
-		if (!realms) {
-			fprintf(stderr, "No memory\n");
-			ret = 1;
-			goto out;
-		}
-
-		/* Get realm information */
-		ret = zbc_report_realms(dev, realms, &nr);
+	/* We can skip zbc_report_nr_realms() call if we receive
+	 * the number from SCSI INQUIRY or ATA LOG page */
+	if (info.zbd_realm_list_length == 0) {
+		ret = zbc_report_nr_realms(dev, &nr_realms);
 		if (ret != 0) {
-			if (ret == -EOPNOTSUPP && flags == 0) {
-				/* Retry forcing SCSI backend */
-				printf("zbc_report_realms returned %d, retrying\n", ret);
-				flags = ZBC_O_DRV_SCSI;
-				zbc_close(dev);
-				continue;
-			}
-			fprintf(stderr, "zbc_report_realms failed %d\n", ret);
+			fprintf(stderr, "zbc_report_nr_realms failed %d\n",
+				ret);
 			ret = 1;
 			goto out;
 		}
-
-		for (i = 0; i < (int)nr; i++) {
-			r = &realms[i];
-			zbc_report_print_realm(&info, r);
-		}
-
-		if (flags != 0)
-			break;
+	} else {
+		nr_realms = info.zbd_realm_list_length;
 	}
+
+	printf("    %u realm%s\n", nr_realms, (nr_realms > 1) ? "s" : "");
+	if (num)
+		goto out;
+
+	if (!nr || nr > nr_realms)
+		nr = nr_realms;
+	if (!nr)
+		goto out;
+
+	/* Allocate realm array */
+	realms = (struct zbc_realm *)calloc(nr, sizeof(struct zbc_realm));
+	if (!realms) {
+		fprintf(stderr, "No memory\n");
+		ret = 1;
+		goto out;
+	}
+
+	/* Get realm information */
+	ret = zbc_report_realms(dev, realms, &nr);
+	if (ret != 0) {
+		fprintf(stderr, "zbc_report_realms failed %d\n", ret);
+		ret = 1;
+		goto out;
+	}
+
+	for (i = 0; i < (int)nr; i++)
+		zbc_report_print_realm(&info, &realms[i]);
 
 out:
 	if (realms)
