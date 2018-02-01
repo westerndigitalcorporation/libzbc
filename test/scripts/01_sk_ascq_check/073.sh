71e03655121040fb91d0609a2f431f0bda23df05
Damien Le Moal <damien.lemoal@wdc.com>
2018-02-01 13:23:19 +0900

    Add write to full zone test
    Introduce the new test 02.073 to check that a device returns illegal
    request/ illegal field in cdb for write operations targeting zones with
    the full condition.
    
    Signed-off-by: Damien Le Moal <damien.lemoal@wdc.com>

---
+++ test/scripts/01_sk_ascq_check/073.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+#
+# This file is part of libzbc.
+#
+# Copyright (C) 2009-2014, HGST, Inc. All rights reserved.
+# Copyright (C) 2016, Western Digital. All rights reserved.
+#
+# This software is distributed under the terms of the BSD 2-clause license,
+# "as is," without technical support, and WITHOUT ANY WARRANTY, without
+# even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+# PURPOSE. You should have received a copy of the BSD 2-clause license along
+# with libzbc. If not, see  <http://opensource.org/licenses/BSD-2-Clause>.
+#
+
+. scripts/zbc_test_lib.sh
+
+zbc_test_init $0 "WRITE full zone" $*
+
+# Expected error code for host-managed devices
+expected_sk="Illegal-request"
+expected_asc="Invalid-field-in-cdb"
+
+# Get drive information
+zbc_test_get_device_info
+
+# Set target zone type
+if [ ${device_model} = "Host-aware" ]; then
+    zone_type="0x3"
+else
+    zone_type="0x2"
+fi
+
+# Get zone information
+zbc_test_get_zone_info
+
+# Search target zone
+zbc_test_search_vals_from_zone_type_and_cond ${zone_type} "0x1"
+
+# Start testing
+zbc_test_run ${bin_path}/zbc_test_finish_zone -v ${device} ${target_slba}
+
+zbc_test_run ${bin_path}/zbc_test_write_zone -v -n 1 ${device} ${target_slba} 8
+
+# Check result
+zbc_test_get_sk_ascq
+
+if [ ${device_model} = "Host-aware" ]; then
+    zbc_test_check_no_sk_ascq
+else
+    zbc_test_check_sk_ascq
+fi
+
+# Post process
+rm -f ${zone_info_file}
+
