westerndigitalcorporation/libzbc

commit bb1e4a6547a739625f724832645a13418e682c09

fix corrupt buffer address when sector count > max
Change zbc_pread/zbc_pwrite to add to the buffer address the number of bytes
read/written rather than something else.  This only affects read/write ops
whose total sector count > zbd_max_rw_sectors.

lib/zbc.c
@@ -560,7 +560,7 @@ ssize_t zbc_pread(struct zbc_device *dev, void *buf,
 			return ret ? ret : -EIO;
 		}
 
-		buf += ret << 9;
+		buf += ret;
 		offset += ret;
 		count -= ret;
 		rd_count += ret;
@@ -615,7 +615,7 @@ ssize_t zbc_pwrite(struct zbc_device *dev, const void *buf,
 			return ret ? ret : -EIO;
 		}
 
-		buf += ret << 9;
+		buf += ret;
 		offset += ret;
 		count -= ret;
 		wr_count += ret;

commit 6ee56dd58025c1d3522c7b67e30a7b1c0db016a6

Fix read/write I/O buffer address handling
Commit bb1e4a6547a7 removed the read/write buffer increment in the
internal loop of zbc_pread() and zbc_pwrite(). This patch restores the
correct buffer increment.

Fixes: bb1e4a6547 "fix corrupt buffer address when sector count > max"

Signed-off-by: David Butterfield <David.Butterfield@wdc.com>
Signed-off-by: Damien Le Moal <damien.lemoal@wdc.com>

lib/zbc.c
@@ -585,7 +585,7 @@ ssize_t zbc_pread(struct zbc_device *dev, void *buf,
 			return ret ? ret : -EIO;
 		}
 
-		buf += ret;
+		buf += ret << 9;
 		offset += ret;
 		count -= ret;
 		rd_count += ret;
@@ -640,7 +640,7 @@ ssize_t zbc_pwrite(struct zbc_device *dev, const void *buf,
 			return ret ? ret : -EIO;
 		}
 
-		buf += ret;
+		buf += ret << 9;
 		offset += ret;
 		count -= ret;
 		wr_count += ret;