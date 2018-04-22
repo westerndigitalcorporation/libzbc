329ca0e946e53d9331c37333cae3d64dda94136b
David Butterfield <David.Butterfield@wdc.com>
2018-04-22 08:12:07 -0600

    Add more test cases
    READ sequential zones boundary violation (with both zones pre-filled)
    CLOSE_ZONE implicit open to empty
    CLOSE_ZONE implicit open to explicit open to empty

---
+++ test/scripts/02_state_machine_check/050.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+#
+# This file is part of libzbc.
+#
+# Copyright (C) 2009-2014, HGST, Inc. All rights reserved.
+# Copyright (C) 2016, Western Digital. All rights reserved.
+#
+# This software is distributed under the terms of the BSD 2-clause license,
+# "as is," without technical support, and WITHOUT ANY WARRANTY, without
+# even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+# PURPOSE. You should have received a copy of the BSD 2-clause license along
+# with libzbc. If not, see  <http://opensource.org/licenses/BSD-2-Clause>.
+#
+
+. scripts/zbc_test_lib.sh
+
+# CLOSE transitions an OPEN zone to EMPTY if the WP is pointing to the start of the zone
+zbc_test_init $0 "CLOSE_ZONE implicit open to explicit open to empty" $*
+
+# Set expected error code
+expected_sk=""
+expected_asc=""
+expected_cond="0x1"
+
+# Get drive information
+zbc_test_get_device_info
+
+if [ ${device_model} = "Host-aware" ]; then
+    zone_type="0x3"
+else
+    zone_type="0x2"
+fi
+
+# Get zone information
+zbc_test_get_zone_info
+
+# Search target LBA
+zbc_test_search_vals_from_zone_type_and_cond ${zone_type} "0x1"
+if [ $? -ne 0 ]; then
+    zbc_test_print_not_applicable "No EMPTY sequential zones"
+fi
+target_lba=${target_slba}
+
+# Start testing
+zbc_test_run ${bin_path}/zbc_test_write_zone -v ${device} ${target_lba} 0
+zbc_test_run ${bin_path}/zbc_test_open_zone -v ${device} ${target_lba}
+zbc_test_run ${bin_path}/zbc_test_close_zone -v ${device} ${target_lba}
+
+# Get SenseKey, ASC/ASCQ
+zbc_test_get_sk_ascq
+
+# Get zone information
+zbc_test_get_zone_info "1"
+
+# Get target zone condition
+zbc_test_search_vals_from_slba ${target_lba}
+
+# Check result
+zbc_test_check_zone_cond
+
+# Post process
+zbc_test_run ${bin_path}/zbc_test_reset_zone ${device} ${target_lba}
+
+rm -f ${zone_info_file}
+
