efa2a5ec53abc349f68f005a46e836945fa99bfd
David Butterfield <David.Butterfield@wdc.com>
2018-04-21 23:05:30 -0600

    Add "N/A" case to avoid command-line syntax errors
    Add a "N/A" case to avoid command-line syntax errors when there are no EMPTY
    sequential zones.

--- test/scripts/00_command_completion_check/070.sh
+++ test/scripts/00_command_completion_check/070.sh
@@ -35,6 +35,9 @@ zbc_test_get_zone_info
 
 # Search target LBA
 zbc_test_search_vals_from_zone_type_and_cond ${zone_type} "0x1"
+if [ $? -ne 0 ]; then
+    zbc_test_print_not_applicable "No EMPTY sequential zones"
+fi
 target_lba=$(( ${target_ptr} ))
 
 # Start testing
