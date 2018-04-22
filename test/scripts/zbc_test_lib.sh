841bcc48e5c399976606111e4a02e9e18bdff931
David Butterfield <David.Butterfield@wdc.com>
2018-04-22 00:32:06 -0600

    call tee in append mode instead of over-writing

--- test/scripts/zbc_test_lib.sh
+++ test/scripts/zbc_test_lib.sh
@@ -130,10 +130,10 @@ function zbc_test_meta_run()
 	local _cmd="$*"
 
 	echo "" >> ${log_file} 2>&1
-	echo "## Executing: ${_cmd}" >> ${log_file} 2>&1
-	echo "" 2>&1 | tee ${log_file} 2>&1
+	echo "### Executing: ${_cmd}" >> ${log_file} 2>&1
+	echo "" 2>&1 | tee -a ${log_file} 2>&1
 
-	${_cmd} 2>&1 | tee ${log_file} 2>&1
+	${_cmd} 2>&1 | tee -a ${log_file} 2>&1
 	ret=${PIPESTATUS[0]}
 
 	return ${ret}
