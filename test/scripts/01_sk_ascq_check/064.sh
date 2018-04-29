33bab9a72e38ee36a6c504c56af76ace774b95cf
David Butterfield <David.Butterfield@wdc.com>
2018-04-29 00:00:04 -0600

    01.064 read from last conv to first active swr

--- test/scripts/01_sk_ascq_check/064.sh
+++ test/scripts/01_sk_ascq_check/064.sh
@@ -2,46 +2,62 @@
 #
 # This file is part of libzbc.
 #
-# Copyright (C) 2018, Western Digital. All rights reserved.
+# Copyright (C) 2009-2014, HGST, Inc. All rights reserved.
+# Copyright (C) 2016, Western Digital. All rights reserved.
 #
 # This software is distributed under the terms of the BSD 2-clause license,
 # "as is," without technical support, and WITHOUT ANY WARRANTY, without
 # even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
 # PURPOSE. You should have received a copy of the BSD 2-clause license along
 # with libzbc. If not, see  <http://opensource.org/licenses/BSD-2-Clause>.
+#
 
 . scripts/zbc_test_lib.sh
 
-zbc_test_init $0 "READ access write pointer zone LBAs starting after write pointer" $*
+zbc_test_init $0 "READ conventional/sequential zone type boundary violation" $*
+
+# Set expected error code - ZBC 4.4.3.4.2 penultimate paragraph
+expected_sk="Illegal-request"
+expected_asc="Attempt-to-read-invalid-data"
 
 # Get drive information
 zbc_test_get_device_info
 
-expected_sk="Illegal-request"
-expected_asc="Attempt-to-read-invalid-data"	# read across WP
+# Get zone information
+zbc_test_get_zone_info
 
-# Search target LBA
-zbc_test_get_wp_zone_or_NA "${ZC_NON_FULL}"
-target_lba=${target_ptr}
+# Search first sequential zone info
+zbc_test_search_last_zone_vals_from_zone_type "0x2"
+func_ret=$?
+if [ ${func_ret} -ne 0 ]; then
+    zbc_test_print_not_applicable "No sequential zone LBA space"
+fi
+reach_lba=${target_slba}
+
+# Search last conventional zone info
+zbc_test_search_last_zone_vals_from_zone_type "0x1"
+func_ret=$?
+if [ ${func_ret} -ne 0 ]; then
+    zbc_test_print_not_applicable "No conventional zone LBA space"
+fi
+
+# Read from last conventional LBA to first sequential LBA
+target_lba=$(( ${target_slba} + ${target_size} - 1 ))
+_nlba=$(( ${reach_lba} - ${target_lba} + 1 ))
 
 # Start testing
-# Write a block starting at the write pointer
-zbc_test_run ${bin_path}/zbc_test_write_zone -v ${device} ${target_lba} ${lblk_per_pblk}
-if [ $? -ne 0 ]; then
-    printf "\nInitial write failed"
-else
-    # Attempt to read an LBA starting beyond the write pointer
-    zbc_test_run ${bin_path}/zbc_test_read_zone -v ${device} $(( ${target_lba} + ${lblk_per_pblk} + 1 )) 1
-fi
+# Read across the boundary from conventional LBA into sequential LBA space
+zbc_test_run ${bin_path}/zbc_test_read_zone -v ${device} ${target_lba} ${_nlba}
 
 # Check result
 zbc_test_get_sk_ascq
 
-if [[ ${unrestricted_read} -ne 0 || ${target_type} != @(${ZT_RESTRICT_READ_GE_WP}) ]]; then
-    zbc_test_check_no_sk_ascq "zone_type=${target_type} URSWRZ=${unrestricted_read}"
+if [ ${device_model} = "Host-aware" ]; then
+    zbc_test_check_no_sk_ascq
 else
-    zbc_test_check_sk_ascq "zone_type=${target_type} URSWRZ=${unrestricted_read}"
+    zbc_test_check_sk_ascq
 fi
 
 # Post process
 rm -f ${zone_info_file}
+
