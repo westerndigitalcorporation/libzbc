7de128e0b8a016639db87ed505c4ba9f2bd7ffcb
David Butterfield <David.Butterfield@wdc.com>
2018-06-05 12:26:27 -0600

    Tests: New test 02.077
    WRITE explicit open to full.
    
    Signed-off-by: David Butterfield <David.Butterfield@wdc.com>

---
+++ test/scripts/02_state_machine_check/077.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+#
+# This file is part of libzbc.
+#
+# Copyright (C) 2018, Western Digital. All rights reserved.
+#
+# This software is distributed under the terms of the BSD 2-clause license,
+# "as is," without technical support, and WITHOUT ANY WARRANTY, without
+# even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+# PURPOSE. You should have received a copy of the BSD 2-clause license along
+# with libzbc. If not, see  <http://opensource.org/licenses/BSD-2-Clause>.
+
+. scripts/zbc_test_lib.sh
+
+zbc_test_init $0 "WRITE explicit open to full" $*
+
+expected_cond="0xe"
+
+# Get drive information
+zbc_test_get_device_info
+
+# Get zone information
+zbc_test_get_zone_info
+
+zbc_test_get_seq_zone_set_cond_or_NA "EOPEN"
+target_lba=${target_slba}
+
+# Start testing
+# Write the entire zone
+zbc_test_run ${bin_path}/zbc_test_write_zone -v ${device} ${target_lba} ${target_size}
+zbc_test_get_sk_ascq
+zbc_test_fail_if_sk_ascq "WRITE failed, zone_type=${target_type}"
+
+# Get zone information
+zbc_test_get_zone_info
+
+# Get target zone condition
+zbc_test_search_vals_from_slba ${target_lba}
+
+# Check result
+zbc_test_check_zone_cond
+
+# Post process
+zbc_test_run ${bin_path}/zbc_test_reset_zone ${device} ${target_lba}
+
+rm -f ${zone_info_file}
