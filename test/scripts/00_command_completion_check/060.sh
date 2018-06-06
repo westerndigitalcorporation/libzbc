01ea5b8b3f3d86e46675c977f959a30a4fb055ad
David Butterfield <David.Butterfield@wdc.com>
2018-06-06 17:15:38 -0600

    Tests: Fix target zone acquisition
    Acquire non-FULL zones for tests that will WRITE.
    
    Signed-off-by: David Butterfield <David.Butterfield@wdc.com>

--- test/scripts/00_command_completion_check/060.sh
+++ test/scripts/00_command_completion_check/060.sh
@@ -33,7 +33,7 @@ fi
 zbc_test_get_zone_info
 
 # Search target LBA
-zbc_test_search_vals_from_zone_type ${zone_type}
+zbc_test_search_vals_from_zone_type_and_ignored_cond ${zone_type} "0xe"
 target_lba=$(( ${target_ptr} ))
 
 nr_sect=$((physical_block_size/512))
