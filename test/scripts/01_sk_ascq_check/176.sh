3fe0e8baca78e34bbf2ded710ed2135f8e38a239
David Butterfield <David.Butterfield@wdc.com>
2018-06-10 14:35:08 -0600

    add reset_zone in Post process

--- test/scripts/01_sk_ascq_check/176.sh
+++ test/scripts/01_sk_ascq_check/176.sh
@@ -45,4 +45,6 @@ else
 fi
 
 # Post process
+zbc_test_run ${bin_path}/zbc_test_reset_zone ${device} ${target_slba}
+zbc_test_run ${bin_path}/zbc_test_reset_zone ${device} $(( ${target_slba} + ${target_size} ))
 rm -f ${zone_info_file}
