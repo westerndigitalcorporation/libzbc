06b6adfa0b62bc4ad3158d9bd85885f3829c339c
David Butterfield <David.Butterfield@wdc.com>
2018-07-06 16:37:53 -0600

    add tests: zone OPs to READ-ONLY or OFFLINE zone
    18 new scripts:
        { OPEN, CLOSE, RESET } * { OFFLINE, RDONLY } * { SMR, SOBR, CONV }
    
    Signed-off-by: David Butterfield <David.Butterfield@wdc.com>

---
+++ test/scripts/05_dh-smr_per_activation/353.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+#
+# This file is part of libzbc.
+#
+# Copyright (C) 2018, Western Digital. All rights reserved.
+#
+# This software is distributed under the terms of the BSD 2-clause license,
+# "as is," without technical support, and WITHOUT ANY WARRANTY, without
+# even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+# PURPOSE. You should have received a copy of the BSD 2-clause license along
+# with libzbc. If not, see  <http://opensource.org/licenses/BSD-2-Clause>.
+
+. scripts/zbc_test_lib.sh
+
+zbc_test_init $0 "RESET an OFFLINE zone" $*
+
+# Get drive information
+zbc_test_get_device_info
+
+expected_sk="Data-protect"
+expected_asc="Zone-is-offline"
+
+# Search target zone
+zbc_test_search_zone_cond_or_NA ${ZC_OFFLINE}
+
+# Start testing
+zbc_test_run ${bin_path}/zbc_test_reset_zone -v ${device} ${target_slba}
+
+# Check result
+zbc_test_get_sk_ascq
+zbc_test_check_sk_ascq "zone_type=${target_type}"
+
+# Post process
+zbc_test_check_failed
+rm -f ${zone_info_file}
