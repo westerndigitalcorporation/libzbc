5f0e430ecc337c1cf481ff4e6c3a58ef03b15764
Damien Le Moal <damien.lemoal@opensource.wdc.com>
2022-11-01 15:02:50 +0900

    lib: ata: Fix ata command names
    Add a "/" in front of the ATA command names so that the overall command
    name including "ATA16" is displayed as "ATA16/xxx" by zbc_sg.c when
    using verbose (debug) mode.
    
    Signed-off-by: Damien Le Moal <damien.lemoal@opensource.wdc.com>

--- lib/zbc_ata.c
+++ lib/zbc_ata.c
@@ -74,40 +74,40 @@ char *zbc_ata_cmd_name(struct zbc_sg_cmd *cmd)
 {
 	switch (cmd->cdb[14]) {
 	case ZBC_ATA_IDENTIFY:
-		return "IDENTIFY";
+		return "/IDENTIFY";
 	case ZBC_ATA_EXEC_DEV_DIAGNOSTIC:
-		return "EXEC DEV DIAGNOSTIC";
+		return "/EXEC DEV DIAGNOSTIC";
 	case ZBC_ATA_READ_LOG_DMA_EXT:
-		return "READ LOG DMA EXT";
+		return "/READ LOG DMA EXT";
 	case ZBC_ATA_SET_FEATURES:
-		return "SET FEATURES";
+		return "/SET FEATURES";
 	case ZBC_ATA_REQUEST_SENSE_DATA_EXT:
-		return "REQUEST SENSE DATA EXT";
+		return "/REQUEST SENSE DATA EXT";
 	case ZBC_ATA_READ_DMA_EXT:
-		return "READ DMA EXT";
+		return "/READ DMA EXT";
 	case ZBC_ATA_WRITE_DMA_EXT:
-		return "WRITE DMA EXT";
+		return "/WRITE DMA EXT";
 	case ZBC_ATA_FLUSH_CACHE_EXT:
-		return "FLUSH CACHE EXT";
+		return "/FLUSH CACHE EXT";
 	case ZBC_ATA_ENABLE_SENSE_DATA_REPORTING:
-		return "ENABLE SENSE DATA REPORTING";
+		return "/ENABLE SENSE DATA REPORTING";
 	case ZBC_ATA_ZAC_MANAGEMENT_IN:
-		return "REPORT ZONES EXT";
+		return "/REPORT ZONES EXT";
 	case ZBC_ATA_ZAC_MANAGEMENT_OUT:
 		switch (cmd->cdb[4]) {
 		case ZBC_ATA_CLOSE_ZONE_EXT_AF:
-			return "CLOSE ZONE EXT";
+			return "/CLOSE ZONE EXT";
 		case ZBC_ATA_FINISH_ZONE_EXT_AF:
-			return "FINISH ZONE EXT";
+			return "/FINISH ZONE EXT";
 		case ZBC_ATA_OPEN_ZONE_EXT_AF:
-			return "OPEN ZONE EXT";
+			return "/OPEN ZONE EXT";
 		case ZBC_ATA_RESET_WRITE_POINTER_EXT_AF:
-			return "RESET WRITE POINTER EXT";
+			return "/RESET WRITE POINTER EXT";
 		}
 		break;
 	}
 
-	return "UNKNOWN COMMAND";
+	return "/UNKNOWN COMMAND";
 }
 
 /**
